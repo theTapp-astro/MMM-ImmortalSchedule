b70697d1168e84713d5c75a5c3a6d934dd9a709f
theTapp-astro <lisa.a.vicars@gmail.com>
2026-08-05 15:18:11 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,49 @@
 {
     module: "MMM-ImmortalSchedule",
     position: "top_left",
+    header: "Immortal Martial Arts",
     config: {
-        updateInterval: 86400000,
+        // Public MyStudio API URL
+        apiUrl: "https://cp.mystudio.io/Api/v2/getClassAppointmentDetails?appointment_for=H&class_scheduler_verion=2&companyid=7491&individual_type=N&limit=7&reg_type_user=U&studentid=0&token=&user_login_type=",
 
+        // Refresh every 5 minutes
+        updateInterval: 86400000, //Once per day
+
+        // Number of days of classes to request/display
+        daysAhead: 7,
+
+        // Leave empty to show ALL classes
         favoriteClasses: [
             "Muay Thai Kickboxing",
             "Muay Thai Sparring & Clinic",
         ],
 
-        daysAhead: 7,
+        // Hide classes that have already started
+        hidePastClasses: true,
+
+        // Display options
+        showDate: true,
+        showDay: true,
+        showTime: true,
         showLocation: true,
         showCapacity: false,
-        hidePastClasses: true
+        showInstructor: false,
+
+        // Optional formatting
+        showHeader: true,
+        showOnlyNextClass: false,
+        maxClasses: 10,
+
+        // Date/time formatting
+        timeFormat: "h:mm A",
+        dateFormat: "ddd, MMM D",
+
+        // CSS class names for easy styling
+        classes: {
+            title: "bright medium",
+            time: "light small",
+            location: "dimmed xsmall",
+            date: "normal small"
+        }
     }
-}
+},
