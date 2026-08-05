75e59223c5ebd7119d7602e362e83e49684f486e
theTapp-astro <lisa.a.vicars@gmail.com>
2026-08-05 15:16:33 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+{
+    module: "MMM-ImmortalSchedule",
+    position: "top_left",
+    config: {
+        updateInterval: 86400000,
+
+        favoriteClasses: [
+            "Muay Thai Kickboxing",
+            "Muay Thai Sparring & Clinic",
+        ],
+
+        daysAhead: 7,
+        showLocation: true,
+        showCapacity: false,
+        hidePastClasses: true
+    }
+}
