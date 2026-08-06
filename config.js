bae50f05cacf9fdbc8cf7a8137ad1ed02b65e880
theTapp-astro <lisa.a.vicars@gmail.com>
2026-08-06 10:07:18 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,46 +1,24 @@
 {
     module: "MMM-ImmortalSchedule",
-    position: "top_left",
-    header: "Immortal Martial Arts",
+    position: "top_right",
 
     config: {
 
-        // Refresh every 5 minutes
-        updateInterval: 86400000, //every 24 hours
-
-        // Your MyStudio company ID
         companyId: 7491,
 
-        // Individual class schedule sources
         sources: [
-
             {
-                appointmentFor: "I",
                 classAppointmentId: 23342
             },
-
             {
-                appointmentFor: "I",
                 classAppointmentId: 23355
             }
-
-            // Add more classes here if desired:
-            // {
-            //     appointmentFor: "I",
-            //     classAppointmentId: 12345
-            // }
-
         ],
 
-        // Display options
-        hidePastClasses: true,
-        showHeader: true,
-        showDate: true,
-        showTime: true,
-        showLocation: true,
-        showCapacity: false,
+        updateInterval: 300000,
 
-        // Maximum number of classes to display
-        maxClasses: 20
+        maxClasses: 20,
+
+        showHeader: true
     }
-},
+}
